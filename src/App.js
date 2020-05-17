68727bac38cf1b929d130bb540525cb942c6f40f
redlogicapps <blejdea.ovidiu@gmail.com>
2020-05-18 01:45:38 +0200

    implemented firebase utils, included ability to store authentichated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -6,20 +6,37 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.component";
 import { auth } from "./firebase/firebase.utils";
+import { createUserProfileDocument } from "./firebase/firebase.utils.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
   unsuscribefromAuth = null;
 
   componentDidMount() {
-    this.unsuscribefromAuth = auth.onAuthStateChanged(user => {this.setState({currentUser: user})});
+    this.unsuscribefromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState(
+            {
+              currentUser: { id: snapshot.id, ...snapshot.data() },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: null });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -29,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,28 @@ const config = {
   appId: "1:422328345973:web:c34899d6304a76f55b9f3c",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  //query firestore to see if it already exists
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
